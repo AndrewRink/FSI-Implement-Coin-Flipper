83dc4fe9198d2beb5f2086273aa16be966ae995b
Andrew <andrewcrink@gmail.com>
2022-08-22 19:59:43 -0400

    Created penny Flip game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,50 @@
 // TODO: Declare any global variables we need
-
+//variables needed: total flipped, heads flipped, tails flipped, heads percent, tails percent
+let totalHeads = 0
+let totalTails = 0
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    // TODO: Add event listener and handler for flip 
+    document.getElementById("flip").addEventListener("click", function() {
+        
+        // determine whether a coin flipped is heads or tails
+        if (Math.random() < 0.5) {
+            document.getElementById("penny-image").src = "assets/images/penny-heads.jpg"
+            document.getElementById("message").textContent = "You Flipped Heads"
 
-    // TODO: Add event listener and handler for flip and clear buttons
+            totalHeads++
+        } else {
+            document.getElementById("penny-image").src = "assets/images/penny-tails.jpg"
+            document.getElementById("message").textContent = "You Flipped Tails"
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+            totalTails++
+        }
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+        let total = totalHeads + totalTails
 
+        let percentHeads = Math.round((totalHeads/total) * 100)
+        let percentTails = Math.round((totalTails/total) * 100)
 
+        document.getElementById("heads").textContent = totalHeads
+        document.getElementById("tails").textContent = totalTails
+        document.getElementById("heads-percent").textContent = percentHeads + "%"
+        document.getElementById("tails-percent").textContent = percentTails + "%"
+    })
+    
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
+
+        document.getElementById("clear").addEventListener("click", function() {
+            totalHeads = 0
+            totalTails = 0
+
+            document.getElementById("heads").textContent = totalHeads
+            document.getElementById("tails").textContent = totalTails
+            document.getElementById("heads-percent").textContent = 0 + "%"
+            document.getElementById("tails-percent").textContent = 0 + "%"
+            
+            document.getElementById("message").textContent= "Let's Get Rolling"
+        })
 
 })
